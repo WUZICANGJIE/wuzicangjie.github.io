c407c2c5ce4e28967de71bd363437f3606313666
WUZICANGJIE <wuzi.ssh@gmail.com>
2025-12-12 10:31:54 +0100

    feat: update profile information and improve localization for multiple languages

--- src/js/main.js
+++ src/js/main.js
@@ -9,23 +9,23 @@ if (window.trustedTypes && window.trustedTypes.createPolicy) {
 (() => {
     const i18n = {
         zh: {
-            title: "孙少瀚 (無字倉頡)",
+            title: "孙少瀚 (无字仓颉)",
             name: "孙少瀚",
-            bio: "Econ student.<br>Homelab/投资/音游",
-            description: "经济学学生，Homelab 爱好者，投资者，音游玩家。",
+            bio: "经济学学生<br>家用服务器/投资/音游",
+            description: "经济学学生，家用服务器爱好者，投资者，音游玩家。",
             saveContact: "添加联系人"
         },
         en: {
-            title: "Shaohan Sun (無字倉頡)",
+            title: "Shaohan Sun (wuzicangjie)",
             name: "Shaohan Sun",
-            bio: "Econ student.<br>Homelab/Investing/Rhythm Games",
-            description: "Econ student, Homelab enthusiast, Investor, Rhythm Game player.",
+            bio: "Econ student<br>Homelab/Investing/Rhythm Games",
+            description: "Econ student. Homelab enthusiast, Investor, Rhythm Game player.",
             saveContact: "Save Contact"
         },
         ja: {
             title: "孫少瀚 (無字倉頡)",
             name: "孫少瀚",
-            bio: "Econ student.<br>自宅サーバー・投資・音ゲー",
+            bio: "経済学の学生<br>自宅サーバー・投資・音ゲー",
             description: "経済学の学生、自宅サーバー愛好家、投資家、音ゲーマー。",
             saveContact: "連絡先を追加"
         }
